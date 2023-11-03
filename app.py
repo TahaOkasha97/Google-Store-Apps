8d18b2189c168a9437e68864845f3f03d94c9d4d
TahaOkasha97 <109887586+TahaOkasha97@users.noreply.github.com>
2023-11-03 08:10:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ st.info("Final Project")
 
 x = ['Rating', 'Reviews', 'Price', 'Last_Updated_Year',
        'Min_Android_Ver',"Category","Content_Rating"]
-x.columns = x.columns.astype(str)
 
 Rating= st.number_input("Enter Rating")
 Reviews= st.number_input("Reviews")
