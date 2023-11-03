5476e9d2f0d9f264123baf04ac35b40c5caa298c
TahaOkasha97 <109887586+TahaOkasha97@users.noreply.github.com>
2023-11-03 10:07:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ df = pd.DataFrame(columns=['Rating', 'Reviews', 'Price', 'Last_Updated_Year',
 
 # append the new row to the DataFrame
 #df = df.append(pd.Series(data, index=df.columns), ignore_index=True)
-df = pd.concat([df, pd.Dataframe(data)], ignore_index=True)
+df = pd.concat([df, pd.Series(data)], ignore_index=True)
 st.write(df)
 #data_scaled = scaler.transform(df)
 #result = model.predict(df)
