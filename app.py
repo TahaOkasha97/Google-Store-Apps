dabf7483fc0f17b100c8a7683c5ddaa56fa17b1c
TahaOkasha97 <109887586+TahaOkasha97@users.noreply.github.com>
2023-11-09 06:16:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ st.title("Google Store Apps Project")
 st.info("A rough prediction of the number of potential users (Installs) for android app")
 
 Rating = st.slider("Rating", min_value=0.0, max_value=5.0, value=4.0, step=0.1)
-Reviews= st.number_input("Reviews",step=1)
 Last_Updated_Year=st.slider("Last Updated Year", min_value=2010, max_value=2024, value=2015, step=1)
 Min_Android_Ver = st.slider("Min Android Ver ",min_value=1.0, max_value=15.0, value=7.0,step=0.1)
+Reviews= st.number_input("Reviews",step=1)
 
 Category_select = st.selectbox("Select Category",{"Category_ART_AND_DESIGN",'Category_AUTO_AND_VEHICLES', 'Category_BEAUTY',
        'Category_BOOKS_AND_REFERENCE', 'Category_BUSINESS', 'Category_COMICS',
@@ -38,8 +38,8 @@ data = [Rating,Reviews,Last_Updated_Year,Min_Android_Ver]
 data = data + Category + Content_Rating
 
 # create a new empty DataFrame with the correct column names
-df = pd.DataFrame(columns=['Rating', 'Reviews', 'Last_Updated_Year',
-       'Min_Android_Ver','Category_AUTO_AND_VEHICLES', 'Category_BEAUTY',
+df = pd.DataFrame(columns=['Rating', 'Last_Updated_Year',
+       'Min_Android_Ver',"Reviews",'Category_AUTO_AND_VEHICLES', 'Category_BEAUTY',
        'Category_BOOKS_AND_REFERENCE', 'Category_BUSINESS', 'Category_COMICS',
        'Category_COMMUNICATION', 'Category_DATING', 'Category_EDUCATION',
        'Category_ENTERTAINMENT', 'Category_EVENTS', 'Category_FAMILY',
